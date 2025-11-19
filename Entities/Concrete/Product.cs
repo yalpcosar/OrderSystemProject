ab3e5f5508c5a9b02bf8777eaf09b0ce548d00b1
Yağızalp Coşar <yalpcosar@hotmail.com>
2025-11-19 16:28:55 +0300

    Product entity düzeltildi

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -8,6 +8,7 @@ namespace Entities.Concrete
     {
         public string Name { get; set; }
         public ESize Size { get; set; }
+        public int PColorId { get; set; }
         public virtual PColor PColor { get; set; }
         public virtual Warehouse Warehouse { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
