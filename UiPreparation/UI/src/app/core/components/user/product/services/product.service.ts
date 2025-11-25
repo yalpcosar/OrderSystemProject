d58e1dff392f668070e3b4ef455fc7eeee8f45a9
Yağızalp Coşar <yalpcosar@hotmail.com>
2025-11-25 17:12:23 +0300

    product.service fixed

--- UiPreparation/UI/src/app/core/components/user/product/services/product.service.ts
+++ UiPreparation/UI/src/app/core/components/user/product/services/product.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+import { ProductDetailDto } from '../models/product-detail-dto';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = environment.getApiUrl + '/products/'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<ProductDetailDto[]> {
+    return this.httpClient.get<ProductDetailDto[]>(this.apiUrl);
+  }
+
+  getProductById(id: number): Observable<ProductDetailDto> {
+    return this.httpClient.get<ProductDetailDto>(this.apiUrl + id);
+  }
+
+  addProduct(product: Product): Observable<any> {
+    return this.httpClient.post(this.apiUrl, product);
+  }
+
+  updateProduct(product: Product): Observable<any> {
+    return this.httpClient.put(this.apiUrl, product);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.httpClient.delete(this.apiUrl + id);
+  }
 }
